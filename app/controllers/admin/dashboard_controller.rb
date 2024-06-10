b847c7a94cc9e426024556e92442b5e82a01f3b7
Yebbenbe <lmtpox@gmail.com>
2024-06-10 09:35:16 -0300

    add admin dashboard info

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,12 @@
 class Admin::DashboardController < ApplicationController
+
+  http_basic_authenticate_with name:ENV['ADMIN_USERNAME'], password:ENV['ADMIN_PASSWORD'], only: [:index,:new]
+
   def show
+
+    @productCount = Product.count
+    @categoryCount = Category.count
+    @product = Product.all
+    @category = Category.all
   end
 end
